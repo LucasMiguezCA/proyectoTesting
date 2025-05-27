1ce59bf5ecc076120022ad157917dfd18721afe4
Lucas Miguez <lucasmiguezca@gmail.com>
2025-05-27 17:25:05 -0300

    Actualizacion 27/5

--- app/Filament/Resources/TareasResource.php
+++ app/Filament/Resources/TareasResource.php
@@ -44,7 +44,7 @@ class TareasResource extends Resource
                     ->label('Fecha de Vencimiento')
                     ->required(),
                 Forms\Components\Repeater::make('subtareas')
-                ->relationship()
+                ->relationship('subtareas', 'IDTarea')
                 ->schema([
                     Forms\Components\TextInput::make('Contenido')
                         ->label('Nombre de la Subtarea')
