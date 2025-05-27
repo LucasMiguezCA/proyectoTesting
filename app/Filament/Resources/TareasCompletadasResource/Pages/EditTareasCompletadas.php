e8b514401f5162ba6d3d8cb485cd79a27e2c7d3e
Lucas Miguez <lucasmiguezca@gmail.com>
2025-05-27 20:57:36 -0300

    Actualizacion 27/5

--- app/Filament/Resources/TareasCompletadasResource/Pages/EditTareasCompletadas.php
+++ app/Filament/Resources/TareasCompletadasResource/Pages/EditTareasCompletadas.php
@@ -9,6 +9,15 @@ use Filament\Resources\Pages\EditRecord;
 class EditTareasCompletadas extends EditRecord
 {
     protected static string $resource = TareasCompletadasResource::class;
+    
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        // Si completada es 0, actualiza estado a 1
+        if (isset($data['completada']) && !$data['completada']) {
+            $data['estado'] = 1;
+        }
+        return $data;
+    }
 
     protected function getHeaderActions(): array
     {
